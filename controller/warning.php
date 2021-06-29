66cb93cbe5fa69440cf7b090115db3daeeaa1ad2
PickDado <davidenotebook@gmail.com>
2021-06-29 20:56:12 +0200

    preparo index e controllore sono passati?

---
+++ controller/warning.php
@@ -0,0 +1,14 @@
+<?php
+
+
+if ($TimeNow>$StarTime&&$TimeNow<$EndTime){
+    echo "ATTENZIONE E' PREVISTA LA RACCOLTA PER QUESTA FASCIA ORARIA";
+}
+
+if($TimeNow<$StarTime){
+    echo "Oggi Devono ancora passare";
+}
+
+if($TimeNow>$EndTime){
+    echo "Oggi sono già passati";
+}
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+
+include("./database/initialize.php");
+
+include("./views/main.php");
+
+include("./views/sorted.php");
