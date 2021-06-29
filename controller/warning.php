7be898f0fa257ba26fdf2a3623a771df28816756
PickDado <davidenotebook@gmail.com>
2021-06-29 22:25:30 +0200

    Modifiche finali

--- controller/warning.php
+++ controller/warning.php
@@ -1,14 +1,15 @@
 <?php
 
+if ($TimeNow != null) {
+    if ($TimeNow > $StarTime && $TimeNow < $EndTime) {
+        echo "ATTENZIONE E' PREVISTA LA RACCOLTA PER QUESTA FASCIA ORARIA";
+    }
 
-if ($TimeNow>$StarTime&&$TimeNow<$EndTime){
-    echo "ATTENZIONE E' PREVISTA LA RACCOLTA PER QUESTA FASCIA ORARIA";
-}
+    if ($TimeNow < $StarTime) {
+        echo "Oggi Devono ancora passare";
+    }
 
-if($TimeNow<$StarTime){
-    echo "Oggi Devono ancora passare";
-}
-
-if($TimeNow>$EndTime){
-    echo "Oggi sono già passati";
-}
+    if ($TimeNow > $EndTime) {
+        echo "Oggi sono già passati";
+    }
+} else echo "Oggi";
